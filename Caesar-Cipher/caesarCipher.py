e1367fabacd1bff5813f741b7b3c7fcb02129e74
0xRizki <rizkiabdelhadi4@gmail.com>
2025-11-26 12:11:01 +0100

    decrypt function by applying negative shift

--- Caesar-Cipher/caesarCipher.py
+++ Caesar-Cipher/caesarCipher.py
@@ -19,8 +19,29 @@ def caesar_cipher (text: str, shift: int) -> str:
     else:
       result_chars.append(ch)
   return ''.join(result_chars)
+
 # decrypt function by applying negative shift
 def caesar_decrypt(text: str, shift: int) -> str:
   return caesar_cipher(text, (-shift)%len(SYMBOLS))
 
+# measure resource 
+process = psutil.Process()
+
+def run_with_measurements(func, *args, **kwargs):
+  # start tracemalloc to measure python allocations
+  tracemalloc.start()
+  # record start time
+  t0_wall = time.perf_counter()
+  t0_proc_times = process.cpu_times().user + process.cpu_times().system
+  
+  try:
+    result = func(*args, **kwargs)
+  finally:
+    t1_wall = time.perf_counter()
+    t1_proc_times = process.cpu_times().user + process.cpu_times().system
+    
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    
+  wall_elapsed = t1_wall - t0_wall
 
