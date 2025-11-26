0deef1e20718e5835f4c9780ca13407237ed1f98
0xRizki <rizkiabdelhadi4@gmail.com>
2025-11-26 11:31:05 +0100

    add Caesar Cipher

--- Caesar-Cipher/caesarCipher.py
+++ Caesar-Cipher/caesarCipher.py
@@ -1,24 +1,8 @@
-#!/usr/bin/env python3 
-
-# Caesar cipher GUI 
+# Caesar Cipher GUI 
 import time 
 import csv 
 import tracemalloc
 import psutil
 from datetime import datetime
-import tkinter as tk 
+import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
-
-#==================== Caesr function ============================#
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
-
-def caesar_encrypt(text: str, shift: int) -> str:
-  result_chars = []
-  for ch in text:
-    if ch in SYMBOLS:
-      symbolIndex = SYMBOLS.find(ch)
-      translatedIndex = (symbolIndex + shift) % len(SYMBOLS)
-      result_chars.append(SYMBOLS[translatedIndex])
-    else:
-      result_chars.append(ch)
-  return ''.join(result_chars)
